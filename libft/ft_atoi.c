acf1eab7b6ec8839cc6ab9dda9cd56a2b31b747a
Lucas Sulzbach rilho <lsulzbac@car15s6.42barcelona.com>
2023-02-08 13:17:26 +0100

    fix atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,30 @@
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 17:59:11 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/12 18:59:23 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2023/02/08 13:07:12 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	init_num(const char *str, int *neg)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t'
+		|| str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
+	{
+		*neg = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	return (i);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -18,22 +37,13 @@ int	ft_atoi(const char *str)
 	int	res;
 	int	neg;
 
-	i = 0;
 	res = 0;
 	neg = 1;
-	while (str[i] != '\0' && (res == 0 || ft_isdigit(str[i])))
+	i = init_num(str, &neg);
+	while (str[i] != '\0')
 	{
-		if (!ft_isdigit(str[i]) && str[i] != ' ' && str[i] != '+'
-			&& str[i] != '-' && str[i] != '\t' && str[i] != '\n'
-			&& str[i] != '\v' && str[i] != '\f' && str[i] != '\r')
+		if (!ft_isdigit(str[i]))
 			break ;
-		if (str[i] == '+' || str[i] == '-')
-		{
-			if (!ft_isdigit(str[i + 1]))
-				break ;
-			if (str[i] == '-')
-				neg = -1;
-		}
 		if (ft_isdigit(str[i]))
 			res = res * 10 + ((str[i] - '0') * neg);
 		i++;
