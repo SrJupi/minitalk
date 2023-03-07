4042fe134937fc07743f72d0cbc028be5a0c9065
Lucas Sulzbach rilho <lsulzbac@car3s4.42barcelona.com>
2023-03-07 18:56:43 +0100

    add error if server not exist

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 13:12:33 by lsulzbac          #+#    #+#             */
-/*   Updated: 2023/02/16 12:27:32 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2023/03/07 18:42:06 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,27 @@ static void	print_status(int counter, int size)
 	ft_putnbr_fd(size, 1);
 }
 
-static void	send_char(int pid, char c)
+static int	send_char(int pid, char c)
 {
 	int	bit;
+	int	sig;
 
 	bit = 0;
 	while (bit < 8)
 	{
 		if (c & (1 << bit))
-		{
-			kill(pid, SIGUSR1);
-			pause();
-		}
+			sig = SIGUSR1;
 		else
-		{
-			kill(pid, SIGUSR2);
-			pause();
-		}
+			sig = SIGUSR2;
+		if (kill(pid, sig) == -1)
+			return (1);
+		pause();
 		bit++;
 	}
+	return (0);
 }
 
-static void	handle_str(int pid, char *str)
+static int	handle_str(int pid, char *str)
 {
 	int	size;
 	int	counter;
@@ -56,13 +55,15 @@ static void	handle_str(int pid, char *str)
 	{
 		if (counter % percent == 0)
 			print_status(counter, size);
-		send_char(pid, *str);
+		if (send_char(pid, *str))
+			return (1);
 		str++;
 		counter++;
 	}
 	print_status(counter--, size);
 	send_char(pid, '\0');
 	ft_putstr_fd("\nMessage Sent!\n", 1);
+	return (0);
 }
 
 static void	handle_sig(int sig)
@@ -91,7 +92,11 @@ int	main(int argc, char **argv)
 	else
 	{
 		ft_putchar_fd('\n', 1);
-		handle_str(ft_atoi(argv[1]), argv[2]);
+		if (handle_str(ft_atoi(argv[1]), argv[2]))
+		{
+			ft_putstr_fd("\nError. Server not found!\n", 1);
+			return (1);
+		}
 	}
 	return (0);
 }
